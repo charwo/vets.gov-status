59f3a9d26cc0c4931fd775f8cd7d0f40a506f0c7
Robert Fairhead <bob@adhocteam.us>
2016-12-07 16:42:09 -0500

    Complete automated update script

--- scripts/run_update.sh
+++ scripts/run_update.sh
@@ -32,14 +32,23 @@ git push -u origin HEAD
 git checkout master
 git branch -d "$(date -I)-ga-data"
 
-# Open the pull request
 GH_TOKEN=$( git config --get github.token )
 
-curl -H "Authorization: token $GH_TOKEN" \
+# Open the pull request and store its number
+ISSUE="$(curl -H "Authorization: token $GH_TOKEN" \
      --data "{
               \"title\": \"$(date -I) automated GA data pull\",
               \"body\": \"Weekly GA data pull\",
               \"head\": \"$(date -I)-ga-data\",
               \"base\": \"master\"
             }" \
-     https://api.github.com/repos/department-of-veterans-affairs/vets.gov-status/pulls
+     https://api.github.com/repos/department-of-veterans-affairs/vets.gov-status/pulls \
+     2> /dev/null | grep \"number\": | sed 's/^\W*number\W*\([0-9][0-9]*\)\W*/\1/')"
+
+# Assign the pull request so an email notification gets sent
+curl -H "Authorization: token $GH_TOKEN" \
+    --data "{
+             \"assignee\": \"$(git config --get github.username)\"
+           }" \
+    -X PATCH \
+    https://api.github.com/repos/department-of-veterans-affairs/vets.gov-status/issues/$ISSUE
